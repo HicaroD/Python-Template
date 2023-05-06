c935a035c8829a2bbe738c3a936e89f027e1428d
HicaroD <hdanrlley1@gmail.com>
2023-05-06 08:20:56 -0300

    updating precommit configuration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.sum import sum
 
+
 if __name__ == "__main__":
     print(sum(1, 2))
--- src/sum.py
+++ src/sum.py
@@ -1,3 +1,2 @@
 def sum(first_number: int, second_number: int):
     return first_number + second_number
-
